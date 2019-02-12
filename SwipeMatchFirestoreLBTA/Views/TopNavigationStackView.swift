c5eed06f518c35a7ed84bbad2e44a5e2f697e311
ovi <faisal.arefin.kazi@gmail.com>
2019-02-12 18:50:05 +0600

    first commit

---
+++ SwipeMatchFirestoreLBTA/Views/TopNavigationStackView.swift
@@ -0,0 +1,21 @@
+//
+//  TopNavigationStackView.swift
+//  SwipeMatchFirestoreLBTA
+//
+//  Created by bs137 on 2/12/19.
+//  Copyright © 2019 bs137. All rights reserved.
+//
+
+import UIKit
+
+class TopNavigationStackView: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
